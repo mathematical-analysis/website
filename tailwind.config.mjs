c1365f43284b19d1ecf10499738016b8ca1d8f39
Cristian Cretu <crisemcr@gmail.com>
2024-06-05 20:47:03 +0300

    a

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,7 +5,9 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {},
-    fontFamily: {},
+    fontFamily: {
+      mono: [...defaultTheme.fontFamily.mono],
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
